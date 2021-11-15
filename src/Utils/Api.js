74b791e0fcbfb58f22aa87bf206fd479a567116f
Devanada <ydevanada@gmail.com>
2021-11-15 22:05:45 +0700

    Added fetch example on login

---
+++ src/Utils/Api.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-anonymous-default-export */
+const prod = "https://disisavail-backend.herokuapp.com";
+const dev = "http://localhost:8081";
+const baseUrl = dev;
+export default {
+  userLogin: baseUrl + "/api/user/login",
+};
