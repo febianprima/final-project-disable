0725eea38435740512e0c5326cb63e98b61b5391
febianprima <arprim13@gmail.com>
2021-11-16 16:39:03 +0700

    apollo

--- src/Routes/App.js
+++ src/Routes/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
@@ -8,16 +11,26 @@ import Contact from "../Pages/Contact";
 import Feed from "../Pages/Feed";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://d-is-able.hasura.app/v1/graphql",
+    headers: {
+        "x-hasura-admin-secret": `WT9sL6pGzoGhrsimm001JI0rMrB37d901kDFnMX5yuKWKmleLnZz2nQjrgiq7IcQ`
+    },
+    cache: new InMemoryCache()
+});
+
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="feed" element={<Feed />} />
-      </Routes>
+      <ApolloProvider client = { client }>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="feed" element={<Feed />} />
+        </Routes>
+      </ApolloProvider>
     </div>
   );
 }
