f7520a97dca0f099dd1d05dd7c108ebb767d6c5c
febianprima <arprim13@gmail.com>
2021-11-19 01:00:31 +0700

    login with heroku

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -14,9 +14,11 @@ import {
   CRow,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
-import { cilLockLocked, cilUser } from "@coreui/icons";
+import { freeSet } from "@coreui/icons";
+import Header from '../Components/Header';
 import axios from "axios";
 import Api from "../Utils/Api";
+import styles from '../Css/Login.module.css';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -48,88 +50,88 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">
-                      Sign In to your account
-                    </p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput
-                        placeholder="Username"
-                        autoComplete="username"
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                        onChange={(e) => setPassword(e.target.value)}
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
+    <>
+      <Header />
+      <div className={`${styles.page} min-vh-100 d-flex flex-row align-items-center`}>
+        <CContainer>
+          <CRow className="justify-content-center">
+            <CCol sm={10}>
+              <CCardGroup>
+                <CCard className= {`p-4 ${styles.login}`}>
+                  <CCardBody>
+                    <CForm>
+                      <h1>Login</h1>
+                      <p className="text-medium-emphasis">
+                        Masuk ke akun Anda!
+                      </p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon content={freeSet.cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          placeholder="Username"
+                          autoComplete="username"
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={freeSet.cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="current-password"
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                      </CInputGroup>
+                      <CRow>
+                        <CCol xs={6}>
+                          <CButton
+                            color="primary"
+                            className={`px-4 ${styles.loginButton}`}
+                            onClick={() => handleSubmit()}
+                          >
+                            Login
+                          </CButton>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0">
+                            Lupa password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                  </CCardBody>
+                </CCard>
+                <CCard
+                  className={`py-5 ${styles.register}`}
+                >
+                  <CCardBody className="text-center">
+                    <div>
+                      <h2>Sign up</h2>
+                      <p>
+                        Belum punya akun?
+                      </p>
+                      <Link to="/register">
                         <CButton
                           color="primary"
-                          className="px-4"
-                          onClick={() => handleSubmit()}
+                          className="mt-3"
+                          active
+                          tabIndex={-1}
                         >
-                          Login
+                          Daftar sekarang!
                         </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard
-                className="text-white bg-primary py-5"
-                style={{ width: "44%" }}
-              >
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton
-                        color="primary"
-                        className="mt-3"
-                        active
-                        tabIndex={-1}
-                      >
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+                      </Link>
+                    </div>
+                  </CCardBody>
+                </CCard>
+              </CCardGroup>
+            </CCol>
+          </CRow>
+        </CContainer>
+      </div>
+    </>
   );
 };
 
