b213b11cf8c95a9a679ff9206bd7f8366693ef11
febianprima <arprim13@gmail.com>
2021-11-24 23:35:20 +0700

    query fixed

--- src/Utils/Query.js
+++ src/Utils/Query.js
@@ -1,39 +1,28 @@
 import { gql } from "@apollo/client";
 
-export const GET_PROFILE = gql`
-  query ($id: Int!) {
-    users(where: { id: { _eq: $id } }) {
-      username
-      profile {
-        firstName
-        lastName
-        status
-        biography
-      }
-      contact {
-        connectionID
-      }
+export const GET_PROFILE = gql `
+query($id: Int!){
+    users (where: {id: {_eq: $id}}){
+        username
+        profile{
+            firstName,
+            lastName,
+            status,
+            biography
+        }
+        contact{
+            connectionID
+        }
     }
-  }
-`;
+}`
 
 export const GET_CONNECTION = gql`
-  query ($id: Int!) {
-    users(where: { id: { _eq: $id } }) {
-      username
-      profile {
-        firstName
-        lastName
-      }
-      contact {
-        userID
-        connectionID
-        connection {
-          username
-          profile {
+query($id: Int!){
+    users (where: {id: {_eq: $id}}){
+        username
+        profile{
             firstName
             lastName
-          }
         }
         contact{
             connection{
@@ -46,48 +35,45 @@ export const GET_CONNECTION = gql`
             }
         }
     }
-  }
-`;
+}`
 
-export const GET_FEED = gql`
-  query ($id: Int!) {
-    users(where: { id: { _eq: $id } }) {
-      username
-      profile {
-        firstName
-        lastName
-        status
-      }
-      contact {
-        connectionID
-      }
-      contents {
-        article
-        user {
-          username
-          profile {
+export const GET_FEED =gql`
+query($id: Int!){
+    users (where: {id: {_eq: $id}}){
+        username
+        profile{
             firstName
             lastName
-          }
+            status
         }
-      }
+        contact{
+            connectionID
+        }
+        contents{
+            article
+            user{
+                username
+                profile{
+                    firstName
+                    lastName
+                }
+            }
+        }
     }
-  }
-`;
+}`
 
-export const GET_CHATROOM = gql`
-  query ($id: Int!) {
-    users(where: { id: { _eq: $id } }) {
-      username
-      email
-      password
-      profile {
-        firstName
-        lastName
-      }
+export const GET_CHATROOM = gql `
+query($id: Int!){
+    users (where: {id: {_eq: $id}}){
+        username
+        email
+        password
+        profile{
+            firstName,
+            lastName
+        }
     }
-  }
-`;
+}`
 
 export const GET_JOBS = gql`
   query {
